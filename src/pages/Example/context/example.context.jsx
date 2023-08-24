244a278f0de7aeaa96cbfb54bb3541fda02afae6
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2023-08-24 01:15:05 -0400

    add comment

--- src/pages/Example/context/example.context.jsx
+++ src/pages/Example/context/example.context.jsx
@@ -2,6 +2,8 @@ import { createContext, useContext, useState } from "react";
 
 export const ExampleContext = createContext();
 
+// I'm in 1:08:53 minute of the video: https://www.youtube.com/watch?v=USm-uiVyTuQ&list=RDLVUSm-uiVyTuQ&start_radio=1&rv=USm-uiVyTuQ&t=1329
+
 export const ExampleProvider = ({ children }) => {
   const [exampleContextValue, setExampleContextValue] = useState("");
 
