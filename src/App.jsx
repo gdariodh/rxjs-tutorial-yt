df3f6010c4392da92fb7bffa13785e59f238483d
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2023-08-24 00:06:32 -0400

    initializing app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ComponentOne from "./components/ComponentOne";
+import ComponentTwo from "./components/ComponentTwo";
+
+function App() {
+  return (
+    <div className="App">
+      <ComponentOne />
+      <ComponentTwo />
+    </div>
+  );
+}
+
+export default App;
